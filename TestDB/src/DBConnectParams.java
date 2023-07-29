bdf161066be6d46bd55efaac527e45d2f5ba9830
dunky-star <dunkygeoffrey39@gmail.com>
2023-07-29 19:45:30 +0300

    JDBC connectivity

---
+++ TestDB/src/DBConnectParams.java
@@ -0,0 +1,6 @@
+public class DBConnectParams {
+    protected static String driver = "com.mysql.jdbc.Driver";
+    protected static String connection = "jdbc:mysql://localhost:3306/basic_jdbc";
+    protected static String user = "student";
+    protected static String password = "cluster@1";
+}
--- TestDB/src/Main.java
+++ TestDB/src/Main.java
@@ -1,5 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        try{
+            Class.forName(DBConnectParams.driver);
+            Connection conn = DriverManager.getConnection(DBConnectParams.connection,
+                    DBConnectParams.user, DBConnectParams.password);
+
+
+        }catch(SQLException e){
+            System.out.println("Something went wrong: " +e.getMessage() );
+        }
     }
 }
