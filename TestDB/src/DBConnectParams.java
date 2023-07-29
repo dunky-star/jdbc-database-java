5f824a3553d2e4a0f4c10e142735d6dedb4f6554
dunky-star <dunkygeoffrey39@gmail.com>
2023-07-29 20:41:15 +0300

    JDBC connectivity

--- TestDB/src/DBConnectParams.java
+++ TestDB/src/DBConnectParams.java
@@ -1,5 +1,5 @@
 public class DBConnectParams {
-    protected static String driver = "com.mysql.cj.jdbc.Driver";
+    // protected static String driver = "com.mysql.cj.jdbc.Driver";
     protected static String connection = "jdbc:mysql://localhost:3306/basic_jdbc";
     protected static String user = "student";
     protected static String password = "cluster@1";
--- TestDB/src/Main.java
+++ TestDB/src/Main.java
@@ -1,14 +1,20 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         try{
-            Class.forName(DBConnectParams.driver);
+            // Class.forName(DBConnectParams.driver); Old method of loading class and not necessary anymore.
             Connection conn = DriverManager.getConnection(DBConnectParams.connection,
                     DBConnectParams.user, DBConnectParams.password);
+            Statement statement = conn.createStatement();
+            statement.execute("SELECT COUNT(DISTINCT song_title) FROM " +
+                    "artist_list WHERE album_name = \"Dragon Fly\"");
 
+            statement.close();
+            conn.close();
 
         }catch(SQLException e){
             System.out.println("Something went wrong: " +e.getMessage() );
