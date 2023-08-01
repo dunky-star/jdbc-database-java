3041abb942f8a2ae7f8067007294d8a89dfa2c62
dunky-star <dunkygeoffrey39@gmail.com>
2023-08-01 16:49:16 +0300

    MySQL - JDBC using constants

--- TestDB/src/DBConnectParams.java
+++ TestDB/src/DBConnectParams.java
@@ -1,6 +1,6 @@
 public class DBConnectParams {
     // protected static String driver = "com.mysql.cj.jdbc.Driver";
-    protected static String connection = "jdbc:mysql://localhost:3306/basic_jdbc";
-    protected static String user = "student";
-    protected static String password = "cluster@1";
+    protected final static String connection = "jdbc:mysql://localhost:3306/basic_jdbc";
+    protected final static String user = "student";
+    protected final static String password = "cluster@1";
 }
