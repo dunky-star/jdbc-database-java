7a361e9b5376186374367f87f8ef044b6ef07c33
dunky-star <dunkygeoffrey39@gmail.com>
2023-08-01 20:58:45 +0300

    MySQL - JDBC using constants and Datasource

--- TestDB/src/model/Datasource.java
+++ TestDB/src/model/Datasource.java
@@ -8,7 +8,19 @@ public class Datasource {
     public static final String PASSWORD = "cluster@1";
 
     public static final String TABLE_ALBUMS = "albums";
+    public static final String COLUMN_ALBUM_ID = "_id";
+    public static final String COLUMN_ALBUM_NAME = "album_name";
+    public static final String COLUMN_ALBUM_ARTIST = "artist";
+
     public static final String TABLE_SONGS = "songs";
+    public static final String COLUMN_SONG_TRACK = "track";
+    public static final String COLUMN_SONG_TITLE = "title";
+    public static final String COLUMN_SONG_ALBUM = "album";
+
+
     public static final String TABLE_ARTISTS = "artists";
+    public static final String COLUMN_ARTIST_ID = "_id";
+    public static final String COLUMN_ARTIST_NAME = "artist_name";
+
     public static final String TABLE_ARTISTS_LIST = "artist_list";
 }
